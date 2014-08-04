4373ff2773cd13adc0132ab594227da0968627e7
ineuron <tbranco@mrc-lmb.cam.ac.uk>
2014-08-04 10:56:06 +0100

    Create tdm2hdf.py

---
+++ tdm2hdf.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+import matplotlib.pylab as plt
+
+import h5py
+
+from cTDMS.cTDMS import TDM_File
+
+
+# Open TDM file
+#fileStr = './python/control_cell_1.TDM'
+fileStr = './python/140124_10Ttx__2__test_pulse__2.tdms'
+
+tdm  = TDM_File(fileStr)
+tdm.open()
+
+# Open HDF5 file
+#fileStrH = './python/control_cell1.hdf5'
+fileStrH = './python/test.hdf5'
+f = h5py.File(fileStrH, 'w')
+
+
+# Get TDM groups
+groups, hgroups = [], []
+
+for groupname in tdm:
+    #groups.append(tdm.Groups[groupname])
+    f.create_group(str(groupname))
+    for channelname in tdm.Groups[groupname]:
+        data = tdm.Groups[groupname][channelname][:]
+        f.create_dataset('/'+str(groupname)+'/'+str(channelname), data=data)
+
+for name in f:
+    print name
+
+# Add attributes to H5 file
+for attr in tdm.attrs:
+    f.attrs[attr] = tdm.attrs[attr]
+f.attrs['TDMS_conversion'] = True
+
+f.close()
+
+# when looping skip *test_pulse*
+
