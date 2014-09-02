da75c9f3269c406392cc2a82435b847dd3ab33fd
ineuron <tiago@trainspare.lmb.internal>
2014-09-02 14:05:01 +0100

    Updates to tdm2hdf.py

--- tdm2hdf.py
+++ tdm2hdf.py
@@ -19,92 +19,49 @@ from cTDMS.cTDMS import TDM_File
 
 
 filedir = 'Z:\data\Experiment Data\imaging\\'
-
 savedir = 'Z:\data\Experiment Data\imaging\H5files\\'
-
 #filedir = 'F:\Analysis\Adam\AdamAnalysis\NPYhrGFP\cells\\'
-
 #savedir = 'F:\Analysis\Adam\AdamAnalysis\NPYhrGFP\cells\H5files\\'
 
 h5fileList = os.listdir(savedir)
 
-
-
+# Go through all files in filedir
 for fname in os.listdir(filedir):
-
   ext = os.path.splitext(fname)[1]
-
   if (ext=='.tdms') | (ext=='.tdm'):
-
-    if ('test_pulse' in fname)==False:
-
+    if ('test_pulse' in fname)==False:   # Skip test_pulse files 
       H5fname =  fname.rstrip('.tdms') + '.hdf5'
-
       #print H5fname, (H5fname in h5fileList)
 
+      # Skipped already converted files
       if (H5fname in h5fileList)==True:
-
         print 'skipped file', H5fname
-
+        
       else:
-
         # Open TDM file
-
-        #fileStr = './python/140124_10Ttx__2__test_pulse__2.tdms'
-
         print fname
-
         tdm  = TDM_File(filedir+fname)
-
         tdm.open()
 
-
-
         # Open HDF5 file
-
-        #H5fname = './python/test.hdf5'
-
         H5fname = savedir + fname.rstrip('.tdms') + '.hdf5'
-
         f = h5py.File(H5fname, 'w')
 
-
-
-        # Get TDM groups
-
+        # Get TDM groups and data from Channels
         groups, hgroups = [], []
-
         for groupname in tdm:
-
-          #groups.append(tdm.Groups[groupname])
-
           f.create_group(str(groupname))
-
           for channelname in tdm.Groups[groupname]:
-
               data = tdm.Groups[groupname][channelname][:]
-
               f.create_dataset('/'+str(groupname)+'/'+str(channelname), data=data)
 
-
-        #for name in f:
-
-        #    print name
-
-
         # Add attributes to H5 file
-
         for attr in tdm.attrs:
-
           f.attrs[attr] = str(tdm.attrs[attr])
-
           if '_rate_kHz' in attr: f.attrs['dt'] = 1./tdm.attrs[attr]
-
         f.attrs['TDMS_conversion'] = True
 
-
-
+        # Close open files
         tdm.close()
-
         f.close()
 
