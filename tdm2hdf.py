1e733c67768d7a4c492c687258d11e9c822b6d44
ineuron <tbranco@mrc-lmb.cam.ac.uk>
2014-09-02 10:50:19 +0100

    Update tdm2hdf.py

--- tdm2hdf.py
+++ tdm2hdf.py
@@ -18,38 +18,93 @@ import h5py
 from cTDMS.cTDMS import TDM_File
 
 
-# Open TDM file
-#fileStr = './python/control_cell_1.TDM'
-fileStr = './python/140124_10Ttx__2__test_pulse__2.tdms'
+filedir = 'Z:\data\Experiment Data\imaging\\'
 
-tdm  = TDM_File(fileStr)
-tdm.open()
+savedir = 'Z:\data\Experiment Data\imaging\H5files\\'
 
-# Open HDF5 file
-#fileStrH = './python/control_cell1.hdf5'
-fileStrH = './python/test.hdf5'
-f = h5py.File(fileStrH, 'w')
+#filedir = 'F:\Analysis\Adam\AdamAnalysis\NPYhrGFP\cells\\'
+
+#savedir = 'F:\Analysis\Adam\AdamAnalysis\NPYhrGFP\cells\H5files\\'
+
+h5fileList = os.listdir(savedir)
 
 
-# Get TDM groups
-groups, hgroups = [], []
 
-for groupname in tdm:
-    #groups.append(tdm.Groups[groupname])
-    f.create_group(str(groupname))
-    for channelname in tdm.Groups[groupname]:
-        data = tdm.Groups[groupname][channelname][:]
-        f.create_dataset('/'+str(groupname)+'/'+str(channelname), data=data)
+for fname in os.listdir(filedir):
 
-for name in f:
-    print name
+  ext = os.path.splitext(fname)[1]
 
-# Add attributes to H5 file
-for attr in tdm.attrs:
-    f.attrs[attr] = tdm.attrs[attr]
-f.attrs['TDMS_conversion'] = True
+  if (ext=='.tdms') | (ext=='.tdm'):
 
-f.close()
+    if ('test_pulse' in fname)==False:
 
-# when looping skip *test_pulse*
+      H5fname =  fname.rstrip('.tdms') + '.hdf5'
+
+      #print H5fname, (H5fname in h5fileList)
+
+      if (H5fname in h5fileList)==True:
+
+        print 'skipped file', H5fname
+
+      else:
+
+        # Open TDM file
+
+        #fileStr = './python/140124_10Ttx__2__test_pulse__2.tdms'
+
+        print fname
+
+        tdm  = TDM_File(filedir+fname)
+
+        tdm.open()
+
+
+
+        # Open HDF5 file
+
+        #H5fname = './python/test.hdf5'
+
+        H5fname = savedir + fname.rstrip('.tdms') + '.hdf5'
+
+        f = h5py.File(H5fname, 'w')
+
+
+
+        # Get TDM groups
+
+        groups, hgroups = [], []
+
+        for groupname in tdm:
+
+          #groups.append(tdm.Groups[groupname])
+
+          f.create_group(str(groupname))
+
+          for channelname in tdm.Groups[groupname]:
+
+              data = tdm.Groups[groupname][channelname][:]
+
+              f.create_dataset('/'+str(groupname)+'/'+str(channelname), data=data)
+
+
+        #for name in f:
+
+        #    print name
+
+
+        # Add attributes to H5 file
+
+        for attr in tdm.attrs:
+
+          f.attrs[attr] = str(tdm.attrs[attr])
+
+          if '_rate_kHz' in attr: f.attrs['dt'] = 1./tdm.attrs[attr]
+
+        f.attrs['TDMS_conversion'] = True
+
+
+
+        tdm.close()
+
+        f.close()
 
